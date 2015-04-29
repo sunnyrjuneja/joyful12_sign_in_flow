c701010e052a383c9eedcaf3224de848eb1b6809
Sunny Juneja <me@sunnyjuneja.com>
2015-04-29 16:34:53 -0700

    Sign up page.

---
+++ db/migrate/20150428233517_add_personal_information_to_users.rb
@@ -0,0 +1,7 @@
+class AddPersonalInformationToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :promo_code, :string
+  end
+end
